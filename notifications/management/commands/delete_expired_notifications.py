30c95effc05a62f84d33dca9b02caade67209658
bedo <bedokhaled66@gmail.com>
2023-06-16 10:21:09 +0300

    NOTIFICATIONS_LIFE_PERIOD  retrieved from settings instead of CLI arg

--- notifications/management/commands/delete_expired_notifications.py
+++ notifications/management/commands/delete_expired_notifications.py
@@ -2,35 +2,30 @@ from datetime import timedelta
 
 from django.core.management.base import BaseCommand
 from django.utils import timezone
+from django.conf import settings
 
 from notifications.models import Notification
 
 
 class Command(BaseCommand):
-    help = "custom django command to delete expired notifications after exceeding a specified time period"
+    help = "custom django command to delete expired notifications after exceeding a certain time period, defined in days"
     
-    def add_arguments(self, parser):
-        parser.add_argument(
-            "--period",
-            type=int,
-            default=30,
-            help="time period in days"
-        )
-
     def handle(self, *args, **options):
-        n_days_provided = options["period"]
+
+        # default to 30 days if value is not configured in settings        
+        notifications_life_period = getattr(settings,"NOTIFICATIONS_LIFE_PERIOD", 30)
         
         print(
-            f"\n start deleting notifications older than {n_days_provided} days ...\n"
+            f"\n start deleting notifications older than {notifications_life_period} days ...\n"
         )
 
         expired_notifications = Notification.objects.exclude(
-            created_at__gt=timezone.now() - timedelta(days=n_days_provided)
+            created_at__gt=timezone.now() - timedelta(days=notifications_life_period)
         )
 
         n_expired_notifications = expired_notifications.count()
         expired_notifications.delete()
 
         print(
-            f"\n--- {n_expired_notifications} expired notifications are deleted from the database ---\n"
+           f"\n--- {n_expired_notifications} expired notifications are deleted from the database ---\n"
         )
