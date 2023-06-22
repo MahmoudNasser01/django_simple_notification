115da3acad3d8f7d367b2c31d42ff04321a23272
MahmoudNasser <mahmoud.nasser@sonono.ch>
2023-06-22 18:21:37 +0300

    enable users queryset

--- demo_project/app/views.py
+++ demo_project/app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
 
-
 def index(request):
     return render(request, 'notification_reciever.html')
--- notifications/handlers.py
+++ notifications/handlers.py
@@ -1,23 +1,34 @@
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
+from django.contrib.auth.models import User
+from django.db.models import QuerySet
 
 from .models import Notification
 from .serializers import NotificationSerializer
 
-channel_layer = get_channel_layer()
-
 
 def send_message(message, user, type: str):
-    notification = Notification.objects.create(user=user, content=message, type=type)
+    channel_layer = get_channel_layer()
+    if isinstance(user, User):
+        # Single user
+        users = [user]
+    elif isinstance(user, QuerySet):
+        # QuerySet of users
+        users = user
+    else:
+        raise ValueError("Invalid user argument. Expected User or QuerySet.")
 
-    async_to_sync(channel_layer.group_send)(
-        get_user_inbox_key(notification.user.id),
-        {
-            'type': 'notification',
-            'notification_type': type,
-            'data': dict(NotificationSerializer(notification).data)
-        }
-    )
+    for recipient in users:
+        notification = Notification.objects.create(user=recipient, content=message, type=type)
+
+        async_to_sync(channel_layer.group_send)(
+            get_user_inbox_key(notification.user.id),
+            {
+                'type': 'notification',
+                'notification_type': type,
+                'data': dict(NotificationSerializer(notification).data)
+            }
+        )
 
 
 def get_user_inbox_key(user_id):
